252e5973e00da5543727b741e00dcff5eb0ab027
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-12 11:56:40 +0530

    Add files via upload

---
+++ streamm.py
@@ -0,0 +1,22 @@
+import streamlit as sl
+import pandas as pd
+sl.write("""
+# My Fist StremLit app
+Hello *World!*
+""")
+
+age=sl.slider("How old are you?",0,100,10)
+sl.write("I am "+str(age) +" years old")
+file1=sl.file_uploader("Choose an excel file only")
+if file1 is not None:
+    df=pd.read_excel("C:\\Users\\Admin\\Downloads\\"+file1.name)
+    sl.write(df)
+    csv = df.to_csv().encode("utf-8")
+    sl.download_button("Download data as CSV",csv,"myfile.csv","text/csv")
+
+dt=sl.date_input("Pick a date")
+sl.write("YOU SELECTED DATE : "+str(dt))
+
+pets=['Cats','Dogs','Cows']
+pet=sl.radio("Pick a pet",pets)
+sl.write("You chose "+pet)
