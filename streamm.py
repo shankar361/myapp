e6a2067dba6533f29fcf05b225c23313995d8550
shankar361 <40621440+shankar361@users.noreply.github.com>
2025-01-12 13:47:40 +0530

    Update streamm.py

--- streamm.py
+++ streamm.py
@@ -1,22 +1,30 @@
 import streamlit as sl
 import pandas as pd
 sl.write("""
-# Shankar's First app
-Hello *World!*, I am Shankar
+# My Fist StremLit app
+Hello *World!*
 """)
+container=sl.empty()
+container2=sl.empty()
+user=container.text_input("Enter Username","Nick")
+passw=container2.text_input("Enter Password",type="password")
+if user == "shankar" and passw == "abc":
+    container.empty()
+    container2.empty()
+    age=sl.slider("How old are you?",0,100,10)
+    sl.write("I am "+str(age) +" years old")
+    file1=sl.file_uploader("Choose an excel file only")
+    if file1 is not None:
+        df=pd.read_excel("C:\\Users\\Admin\\Downloads\\"+file1.name)
+        sl.write(df)
+        csv = df.to_csv().encode("utf-8")
+        sl.download_button("Download data as CSV",csv,"myfile.csv","text/csv")
 
-age=sl.slider("How old are you?",0,100,10)
-sl.write("I am "+str(age) +" years old")
-file1=sl.file_uploader("Choose an excel file only")
-if file1 is not None:
-    df=pd.read_excel("C:\\Users\\Admin\\Downloads\\"+file1.name)
-    sl.write(df)
-    csv = df.to_csv().encode("utf-8")
-    sl.download_button("Download data as CSV",csv,"myfile.csv","text/csv")
+    dt=sl.date_input("Pick a date")
+    sl.write("YOU SELECTED DATE : "+str(dt))
 
-dt=sl.date_input("Pick a date")
-sl.write("YOU SELECTED DATE : "+str(dt))
-
-pets=['Cats','Dogs','Cows']
-pet=sl.radio("Pick a pet",pets)
-sl.write("You chose "+pet)
+    pets=['Cats','Dogs','Cows']
+    pet=sl.radio("Pick a pet",pets)
+    sl.write("You chose "+pet)
+else:
+    sl.error("Invalid Credetials")
